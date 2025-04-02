b367a3127b4da9f64f49b07fa2d4155642f1749c
timeomnd <timeo.menard@isen-ouest.yncrea.fr>
2025-04-02 15:35:19 +0200

    sdqs

---
+++ cmake-build-debug/CMakeFiles/ProjetC_.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/ProjetC_.dir/main.cpp.o"
+  "CMakeFiles/ProjetC_.dir/main.cpp.o.d"
+  "ProjetC_"
+  "ProjetC_.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/ProjetC_.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
