e1d86f15bc431097387466b8360bd44ad9a06596
RomainLCBT <romain.lecaillon--blot@isen-ouest.yncrea.fr>
2025-04-12 08:43:24 +0200

    added player class

--- Player.h
+++ Player.h
@@ -6,3 +6,29 @@
 #define PLAYER_H
 
 #endif //PLAYER_H
+
+struct position
+{
+    int x, y;
+};
+
+class Player{
+
+    private : 
+        position pos;
+        float orientation;
+        int pv;
+    
+    public :
+
+        void setPosition(position NewPos){};
+        void setPosition(position NewPos, float NewOrientation){};
+        void setPv(int NewPv){};
+
+        position getPosition(){};
+        float getOrientation(){};
+        int getPv(){};
+
+
+
+};
