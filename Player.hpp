f02bf0532cf023f8562578c575f98e7f7404cf0a
RomainLCBT <romain.lecaillon--blot@isen-ouest.yncrea.fr>
2025-04-12 13:28:50 +0200

    v1 nils implanté

--- Player.hpp
+++ Player.hpp
@@ -5,14 +5,15 @@
 #include <QObject>
 #include <QKeyEvent>
 #include <QTimer>
+#include <QPixmap>
 
 class Player : public QObject, public QGraphicsPixmapItem {
     Q_OBJECT
 
 public:
     Player(QGraphicsItem* parent = nullptr);
-
     void keyPressEvent(QKeyEvent* event) override;
+    void keyReleaseEvent(QKeyEvent* event) override;
     void setSpeed(int s);
     int getSpeed() const;
 
@@ -24,6 +25,10 @@ private:
     int dx;
     int dy;
     QTimer* movementTimer;
+    QPixmap spriteUp;    // Sprite pour la direction haut
+    QPixmap spriteDown;  // Sprite pour la direction bas
+    QPixmap spriteLeft;  // Sprite pour la direction gauche
+    QPixmap spriteRight; // Sprite pour la direction droite
 };
 
 #endif // PLAYER_HPP
