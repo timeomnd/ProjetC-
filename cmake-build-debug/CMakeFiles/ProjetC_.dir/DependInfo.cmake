b58e198699be2a70f99bb4eef3bc9c686210142b
timeomnd <timeo.menard@isen-ouest.yncrea.fr>
2025-04-16 12:58:59 +0200

    push obligatoire de merde

---
+++ cmake-build-debug/CMakeFiles/ProjetC_.dir/DependInfo.cmake
@@ -0,0 +1,23 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "/mnt/c/Users/timti/Desktop/ProjetC-/Boss.cpp" "CMakeFiles/ProjetC_.dir/Boss.cpp.o" "gcc" "CMakeFiles/ProjetC_.dir/Boss.cpp.o.d"
+  "/mnt/c/Users/timti/Desktop/ProjetC-/MainWindow.cpp" "CMakeFiles/ProjetC_.dir/MainWindow.cpp.o" "gcc" "CMakeFiles/ProjetC_.dir/MainWindow.cpp.o.d"
+  "/mnt/c/Users/timti/Desktop/ProjetC-/MyScene.cpp" "CMakeFiles/ProjetC_.dir/MyScene.cpp.o" "gcc" "CMakeFiles/ProjetC_.dir/MyScene.cpp.o.d"
+  "/mnt/c/Users/timti/Desktop/ProjetC-/Player.cpp" "CMakeFiles/ProjetC_.dir/Player.cpp.o" "gcc" "CMakeFiles/ProjetC_.dir/Player.cpp.o.d"
+  "/mnt/c/Users/timti/Desktop/ProjetC-/main.cpp" "CMakeFiles/ProjetC_.dir/main.cpp.o" "gcc" "CMakeFiles/ProjetC_.dir/main.cpp.o.d"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ cmake-build-debug/CMakeFiles/ProjetC_.dir/compiler_depend.make
@@ -0,0 +1,2 @@
+# Empty compiler generated dependencies file for ProjetC_.
+# This may be replaced when dependencies are built.
