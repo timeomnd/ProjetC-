04fb0f33a86123499aedc40878943fb46edc6ec0
timeomnd <timeo.menard@isen-ouest.yncrea.fr>
2025-05-04 19:20:07 +0200

    map

--- assets/tileset1.tsx
+++
@@ -1,4 +0,0 @@
-<?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.10" tiledversion="1.11.2" name="tileset1" tilewidth="96" tileheight="192" tilecount="16" columns="4">
- <image source="../../../Downloads/A5.png" width="384" height="768"/>
-</tileset>
--- assets/tilesetpng.tsx
+++
@@ -1,4 +0,0 @@
-<?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.10" tiledversion="1.11.2" name="tilesetpng" tilewidth="32" tileheight="32" tilecount="8" columns="4">
- <image source="../../../Downloads/tilesetpng.png" width="145" height="85"/>
-</tileset>
