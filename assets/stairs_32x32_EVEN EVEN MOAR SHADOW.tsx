00872cbeca8c46a12cf2a1c4dee6b533317bb925
timeomnd <timeo.menard@isen-ouest.yncrea.fr>
2025-05-05 14:17:38 +0200

    f

--- assets/stairs_32x32_EVEN EVEN MOAR SHADOW.tsx
+++
@@ -1,4 +0,0 @@
-<?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.10" tiledversion="1.11.2" name="stairs_32x32_EVEN EVEN MOAR SHADOW" tilewidth="32" tileheight="32" tilecount="1" columns="1">
- <image source="../../../Downloads/stairs_32x32_EVEN EVEN MOAR SHADOW.png" width="32" height="32"/>
-</tileset>
